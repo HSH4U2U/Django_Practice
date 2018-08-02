09dc0432c4afaed4ef4ecffe6067a66331662202
HSH4U2U <hsh700788@naver.com>
2018-08-02 23:18:41 +0900

    dogo로url 설정 연습

--- dojo/views.py
+++ dojo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def mysum(request, numbers):
+    # result = sum(map(int, numbers.split("/")))
+    result = sum(map(lambda s: int(s or 0), numbers.split("/")))
+    return HttpResponse(result)
