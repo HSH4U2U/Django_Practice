e1043e9608341d158018b4076680efc86314e9db
HSH4U2U <hsh700788@naver.com>
2018-08-03 01:31:01 +0900

    dogo로 함수 기반 뷰의 3가지 패턴(1가지 생략)

--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 def mysum(request, numbers):
@@ -10,3 +10,25 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name,age))
+
+
+def post_list1(request):
+    name = '상현'
+    return  HttpResponse('''
+    <h1>AskDjango</h1>
+    <p>{name}</p>
+    <p>여러분은 파이썬&장고 마스터입니다!</p>
+    '''.format(name=name))
+
+
+def post_list2(request):
+    name = '상현'
+    return render(request, 'dojo/post_list.html', {'name': name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azu'],
+        }, json_dumps_params={'ensure_ascii': False}
+    )
