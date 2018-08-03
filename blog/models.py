1f962fc5d3682f7e05409fc9d34918cc44a472c1
HSH4U2U <hsh700788@naver.com>
2018-08-03 15:11:45 +0900

    blog.models.Post 모델에 옵션 필드인 lnglat/tags 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+
 
 # Create your models here.
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
+
+
 class Post(models.Model):
     # title = models.CharField(max_length=100)
     # title = models.CharField(max_length=100,
@@ -11,5 +19,9 @@ class Post(models.Model):
     #     ))
     title = models.CharField(max_length=100, verbose_name='제목',help_text='포스팅 제목을 입력해주세요. 100자 내외')
     content = models.TextField(verbose_name='내용')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True,
+        validators=[lnglat_validator],
+        help_text='위도,경도 포멧으로 받겠습니다.')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
