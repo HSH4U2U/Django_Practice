bb932e83ce3a45c9308e99a430474b26015a60bf
HSH4U2U <hsh700788@naver.com>
2018-08-05 01:24:47 +0900

    blog.models.Tag blank로 옵션화

--- blog/models.py
+++ blog/models.py
@@ -31,7 +31,7 @@ class Post(models.Model):
         validators=[lnglat_validator],
         help_text='위도,경도 포멧으로 받겠습니다.')
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    tag_set = models.ManyToManyField('Tag')
+    tag_set = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
