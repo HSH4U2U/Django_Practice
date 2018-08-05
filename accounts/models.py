d2d2ff6ccf2d64bdf6fbf7d77cf12dfa2df08b04
HSH4U2U <hsh700788@naver.com>
2018-08-05 12:28:57 +0900

    account.models.Profile 모델 생성/ admin 등록

--- accounts/models.py
+++ accounts/models.py
@@ -2,3 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.DO_NOTHING,)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
